3d9e70f373ae7aa16a45b82a893c1220ecaa3759
josetor101 <josetordecilla800@gmail.com>
2024-11-17 22:18:12 -0500

    Migrations issue solved

--- database/migrations/2024_09_10_152620_create_instruments_table.php
+++ database/migrations/2024_09_10_152620_create_instruments_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
                 $table->string('category');
                 $table->string('brand');
                 $table->integer('price');
-                $table->float('reviewSum')->default(0); // Establece valor por defecto
-                $table->integer('numberOfReviews')->default(0); // Establece valor por defecto
+                $table->float('reviewSum')->default(0); 
+                $table->integer('numberOfReviews')->default(0);
                 $table->integer('quantity')->default(1);
                 $table->string('image');
                 $table->timestamps();
