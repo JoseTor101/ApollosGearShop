e8ddfa18fe8d314e63cb3d3b41358eac991789f1
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-11-19 19:38:53 -0500

    fix: lan in pdf and csv

--- app/Interfaces/PdfGeneratorInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Interfaces;
-
-interface PdfGeneratorInterface
-{
-    public function generate(string $view, array $data): string;
-}
--- app/Services/CsvGeneratorService.php
+++ app/Services/CsvGeneratorService.php
@@ -10,22 +10,35 @@ class CsvGeneratorService implements DocumentGeneratorInterface
     {
         $output = fopen('php://temp', 'r+');
 
-        // Encabezados del CSV
-        fputcsv($output, ['Product', 'Quantity', 'Price']);
+        fputcsv($output, [
+            __('order.order_id'),
+            __('order.customer'),
+            __('order.creation_date'),
+            __('order.delivery_date'),]);
+        fputcsv($output, [
+            $data['order']->getId(),
+            $data['order']->getUser()->getName(),
+            $data['order']->getCreatedAt(),
+            $data['order']->getDeliveryDate(),
+        ]);
 
-        // Itera sobre los ítems de la orden
-        if (!empty($data['order']->itemInOrders)) {
-            foreach ($data['order']->itemInOrders as $item) {
-                fputcsv($output, [
-                    $item->getType(),
-                    $item->getQuantity(),
-                    $item->getPrice(),
-                ]);
-            }
-        } else {
-            // Mensaje en caso de que no haya ítems
-            fputcsv($output, ['No items found', '', '']);
+        fputcsv($output, []);
+
+        fputcsv($output, [
+            __('order.product'),
+            __('order.quantity'),
+            __('order.price'),]);
+
+        foreach ($data['order']->itemInOrders as $item) {
+            fputcsv($output, [
+                $item->getType(),
+                $item->getQuantity(),
+                $item->getPrice(),
+            ]);
         }
+
+        fputcsv($output, []);
+        fputcsv($output, [__('order.total'), '', $data['order']->getCustomTotalPrice()]);
 
         rewind($output);
         $csvContent = stream_get_contents($output);
